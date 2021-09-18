a3e35f1d7b4bf751545b9045f9629480805f6d01
marsanderson <82884072+marsanderson@users.noreply.github.com>
2021-09-18 12:18:49 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {ProductProvider} from './context';
 import reportWebVitals from './reportWebVitals';
+// import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ProductProvider>
+  <Router>
+    
     <App />
-  </React.StrictMode>,
+  </Router>
+  </ProductProvider>,
   document.getElementById('root')
 );
 
@@ -15,3 +21,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+// serviceWorker.unregister();
