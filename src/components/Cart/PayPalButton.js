a73ce495d52ae92dd971801e1e97e565a68ad04a
marsanderson <82884072+marsanderson@users.noreply.github.com>
2021-09-30 21:03:10 -0400

    trying to fix paypal glitch3

--- src/components/Cart/PayPalButton.js
+++ src/components/Cart/PayPalButton.js
@@ -7,7 +7,7 @@ export default class MyApp extends React.Component {
             // Congratulation, it came here means everything's fine!
             		console.log("The payment was successful!", payment);
                     this.props.clearCart();
-                    this.props.history.push('/emptycart');
+                    this.props.history.push('/');
             		// You can bind the "payment" object's value to your state or props or whatever here, please see below for sample returned data
         }
  
@@ -31,6 +31,8 @@ export default class MyApp extends React.Component {
  
         const client = {
             sandbox:    process.env.REACT_APP_APP_ID,
+            // sandbox:    "AYImD41QncRkqmiPYaGQxMjvYbUE5duU8G3vimZfnuEpjONhkhCUv0DLycbiwS0TzvMoBd1zb6t-RK-u",
+            production: "YOUR-PRODUCTION-APP-ID",
             production: process.env.REACT_APP_APP_ID,
         }
         // In order to get production's app-ID, you will have to send your app to Paypal for approval first
