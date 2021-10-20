7f85545acacc483a310dc811921ce42acb48d252
marsanderson <82884072+marsanderson@users.noreply.github.com>
2021-10-19 21:53:31 -0400

    add owners bio

--- src/components/About.js
+++ src/components/About.js
@@ -19,13 +19,20 @@ export default class About extends Component {
           <div className="col-lg-5">
             <h1 className="col-10 mx-auto text-center text-title">A Little Something About Kita!</h1>
             <p className="h5">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed varius facilisis quam 
-            id luctus. Proin eu metus at orci consectetur porta eget nec felis. Praesent turpis 
-            ex, iaculis sed egestas quis, rhoncus in diam. Quisque ac justo quis lorem vulputate 
-            ricies. Praesent imperdiet, est eget rutrum pulvinar, felis ligula facilisis eros, 
-            sed bibendum augue lacus ac odio. Ut nisi enim, viverra non interdum eget, lacinia 
-            in lorem. Phasellus lorem nibh, molestie nec imperdiet ac, consequat id arcu.
+            When I grew tired with what seemed like year-round sinus and congestion issues and the 
+            OTC options seemed to stop working every two months, I began to do research. I found 
+            natural ways that could ease not only the symptoms of sinus congestion, but other ailments 
+            such as anxiety, stress, relief from headaches, nausea and even could possibly uplift the 
+            mood in your home. I founded Oh So Green, to create products that uses aromatherapy, botanicals, 
+            and essential oil based Room Sprays, Shower Steamers, Tinctures, and Salves. With this never-ending 
+            research I then found a creative way in combining these elements. I created Oh So Green, an Online 
+            Herbal Apothecary Store, to share my creations with the world. Oh So Green is committed to offering 
+            the best natural and creative ways to help provide serenity for you and your home.
             </p>
+            <h2 className="col-10 mx-auto text-center text-title">“……because YOUR home and YOUR vessel is 
+            YOUR Sanctuary and it should always feel like the most peaceful, beautiful, and high vibrational 
+            place on earth.” ~Nekita</h2>
+            <h2 className="col-10 mx-auto text-center"> ~Nekita</h2>
           </div>
         </div>
       </div>
